3c251a7466095320ce6a8e1ed88648c3fccf7d1c
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-08-28 14:33:36 +0300

    add userSettings adaptivity

--- src/pages/MainPage/Cabinet/UserSettings/UserSettings.style.tsx
+++ src/pages/MainPage/Cabinet/UserSettings/UserSettings.style.tsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 import FormInput from "../../../../components/FormInput/FormInput";
+import Hint from "../../../../components/Hint/Hint";
+
+export const StyledSettings = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-radius: ${(props) => props.theme.borderRadius};
+  background-color: ${(props) => props.theme.colors.primary};
+`;
 
 export const Title = styled.h2`
   font-size: 16px;
@@ -16,6 +27,7 @@ export const Row = styled.div`
 
   @media screen and (max-width: 1169px) {
     gap: 10px;
+    flex-wrap: wrap;
   }
 
   @media screen and (max-width: 680px) {
@@ -33,27 +45,52 @@ export const Cell = styled.div`
 
   @media screen and (max-width: 1169px) {
     max-width: 300px;
-    min-width: 215px;
+    min-width: 190px;
     flex: 1 1 auto;
+    width: fit-content;
   }
 
   @media screen and (max-width: 680px) {
-    max-width: 370px;
+    max-width: 100%;
+    width: 100%;
   }
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+
   border-radius: ${(props) => props.theme.borderRadius};
   background-color: ${(props) => props.theme.colors.primary};
   padding: 18px;
+
+  @media screen and (max-width: 1169px) {
+    padding: 9px;
+  }
 
   @media screen and (max-width: 680px) {
     max-width: 340px;
   }
 `;
 
-export const StyledFormInput = styled(FormInput)`
+export const SettingInput = styled(FormInput)`
   max-width: 270px;
+
+  @media screen and (max-width: 1169px) {
+    min-width: 190px;
+    width: auto;
+  }
+
+  @media screen and (max-width: 680px) {
+    max-width: 100%;
+  }
+`;
+
+export const SettingHint = styled(Hint)`
+  @media screen and (max-width: 680px) {
+    font-size: 12px;
+    display: inline-flex;
+    justify-content: flex-start;
+    gap: 3px;
+  }
 `;
--- src/pages/MainPage/Cabinet/UserSettings/UserSettings.tsx
+++ src/pages/MainPage/Cabinet/UserSettings/UserSettings.tsx
@@ -1,9 +1,15 @@
 import { useCallback, useEffect, useState } from "react";
 import Checkbox from "../../../../components/Checkbox/Checkbox";
-import FormInput from "../../../../components/FormInput/FormInput";
-import Hint from "../../../../components/Hint/Hint";
 import InlineLink from "../../../../components/InlineLink/InlineLink";
-import { Cell, Form, Row, StyledFormInput, Title } from "./UserSettings.style";
+import {
+  Cell,
+  Form,
+  Row,
+  SettingHint,
+  SettingInput,
+  StyledSettings,
+  Title,
+} from "./UserSettings.style";
 import { DataForm, ValidationResult } from "../../../../types/types";
 import Submit from "../../../../components/Buttons/Submit/Submit";
 import { isStartFormValid } from "../../../../utils/validation";
@@ -86,127 +92,133 @@ export default function UserSettings() {
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Title>Введите контактные данные</Title>
-      <Hint>
-        {" "}
-        Уже покупаете у нас? Войдите в{" "}
-        <InlineLink to="/login">личный кабинет</InlineLink>
-      </Hint>
-      <Row>
-        <FormInput
-          type="text"
-          placeholder="Имя"
-          name="firstName"
-          required
-          onChange={useCreateOnChange("name")}
-          className={
-            isCorrect.badFields.find((item) => item === "name") ? "error" : ""
-          }
-          value={data.name}
-        />
-        <FormInput
-          type="tel"
-          placeholder="Телефон"
-          name="phone"
-          required
-          onChange={useCreateOnChange("phone")}
-          className={
-            isCorrect.badFields.find((item) => item === "phone") ? "error" : ""
-          }
-          value={data.phone}
-        />
-        <FormInput
-          type="email"
-          placeholder="Email"
-          required
-          onChange={useCreateOnChange("email")}
-          className={
-            isCorrect.badFields.find((item) => item === "email") ? "error" : ""
-          }
-          value={data.email}
-        />
-      </Row>
-      <Checkbox
-        id="agent"
-        onChange={useCreateOnChange("isAgent")}
-        isChecked={data.isAgent}
-      >
-        Я представитель юридического лица или ИП
-      </Checkbox>
-      <Hint>
-        Данной организации еше нет в нашей базе. Пожалуйста, введите по ней
-        информацию.
-      </Hint>
-      <Row>
-        <StyledFormInput
-          type="text"
-          placeholder="Название организации*"
-          name="organization"
-          required
-          onChange={useCreateOnChange("organization")}
-          className={
-            isCorrect.badFields.find((item) => item === "organization")
-              ? "error"
-              : ""
-          }
-          value={data.organization}
-        />
-        <Cell>
-          <StyledFormInput
+    <StyledSettings>
+      <Form onSubmit={onSubmit}>
+        <Title>Введите контактные данные</Title>
+        <SettingHint>
+          {" "}
+          Уже покупаете у нас? Войдите в{" "}
+          <InlineLink to="/login">личный кабинет</InlineLink>
+        </SettingHint>
+        <Row>
+          <SettingInput
             type="text"
-            placeholder="Юридический адрес*"
-            name="address"
+            placeholder="Имя"
+            name="firstName"
             required
-            onChange={useCreateOnChange("uAddress")}
+            onChange={useCreateOnChange("name")}
             className={
-              isCorrect.badFields.find((item) => item === "uAddress")
+              isCorrect.badFields.find((item) => item === "name") ? "error" : ""
+            }
+            value={data.name}
+          />
+          <SettingInput
+            type="tel"
+            placeholder="Телефон"
+            name="phone"
+            required
+            onChange={useCreateOnChange("phone")}
+            className={
+              isCorrect.badFields.find((item) => item === "phone")
                 ? "error"
                 : ""
             }
-            value={data.uAddress}
+            value={data.phone}
           />
-          <Checkbox
-            id="isAddressEquals"
-            onChange={useCreateOnChange("isAddressEquals")}
-            isChecked={data.isAddressEquals}
-          >
-            Совпадает с физическим
-          </Checkbox>
-        </Cell>
-        {!data.isAddressEquals ? (
-          <StyledFormInput
-            type="text"
-            placeholder="Физический адрес*"
-            name="address"
+          <SettingInput
+            type="email"
+            placeholder="Email"
             required
-            onChange={fAddressOnChange}
+            onChange={useCreateOnChange("email")}
             className={
-              isCorrect.badFields.find((item) => item === "fAddress")
+              isCorrect.badFields.find((item) => item === "email")
                 ? "error"
                 : ""
             }
-            value={data.fAddress !== null ? data.fAddress : ""}
+            value={data.email}
           />
-        ) : undefined}
-      </Row>
-      <Row>
-        <StyledFormInput
-          type="number"
-          placeholder="ИНН*"
-          name="INN"
-          required
-          minLength={8}
-          maxLength={8}
-          onChange={useCreateOnChange("INN")}
-          className={
-            isCorrect.badFields.find((item) => item === "inn") ? "error" : ""
-          }
-          value={data.INN}
-        />
-      </Row>
+        </Row>
+        <Checkbox
+          id="agent"
+          onChange={useCreateOnChange("isAgent")}
+          isChecked={data.isAgent}
+        >
+          Я представитель юридического лица или ИП
+        </Checkbox>
+        <SettingHint>
+          Данной организации еше нет в нашей базе. Пожалуйста, введите по ней
+          информацию.
+        </SettingHint>
+        <Row>
+          <SettingInput
+            type="text"
+            placeholder="Название организации*"
+            name="organization"
+            required
+            onChange={useCreateOnChange("organization")}
+            className={
+              isCorrect.badFields.find((item) => item === "organization")
+                ? "error"
+                : ""
+            }
+            value={data.organization}
+          />
+          <Cell>
+            <SettingInput
+              type="text"
+              placeholder="Юридический адрес*"
+              name="address"
+              required
+              onChange={useCreateOnChange("uAddress")}
+              className={
+                isCorrect.badFields.find((item) => item === "uAddress")
+                  ? "error"
+                  : ""
+              }
+              value={data.uAddress}
+            />
+            <Checkbox
+              id="isAddressEquals"
+              onChange={useCreateOnChange("isAddressEquals")}
+              isChecked={data.isAddressEquals}
+            >
+              Совпадает с физическим
+            </Checkbox>
+          </Cell>
+          {!data.isAddressEquals ? (
+            <SettingInput
+              type="text"
+              placeholder="Физический адрес*"
+              name="address"
+              required
+              onChange={fAddressOnChange}
+              className={
+                isCorrect.badFields.find((item) => item === "fAddress")
+                  ? "error"
+                  : ""
+              }
+              value={data.fAddress !== null ? data.fAddress : ""}
+            />
+          ) : undefined}
+        </Row>
+        <Row>
+          <SettingInput
+            type="number"
+            placeholder="ИНН*"
+            name="INN"
+            required
+            minLength={8}
+            maxLength={8}
+            onChange={useCreateOnChange("INN")}
+            className={
+              isCorrect.badFields.find((item) => item === "inn") ? "error" : ""
+            }
+            value={data.INN}
+          />
+        </Row>
 
-      <Submit value="Изменить" />
-    </Form>
+        <Submit value="Изменить" />
+      </Form>
+    </StyledSettings>
   );
 }
